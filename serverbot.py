c343c4be3deaff8cfaf7d44e46de8724f19f5d76
pavel <hircumg@gmail.com>
2020-04-30 15:05:12 +0300

    added try catch

--- serverbot.py
+++ serverbot.py
@@ -30,7 +30,16 @@ bot = telebot.TeleBot(API_TOKEN)
 server = flask.Flask(__name__)
 
 LOGGER.info("Loading model started")
-translator, paraphraser = loadmodel()
+translator, paraphraser = None, None
+try:
+    translator, paraphraser = loadmodel()
+    # if error occurred send 0 score for submission
+    except Exception as e:
+
+        LOGGER.error(traceback.format_tb(sys.exc_info()[-1]))
+        LOGGER.info(traceback.format_tb(sys.exc_info()[-1]))
+        LOGGER.info(f"error: {e}")
+
 LOGGER.info("Loading model finished")
 
 
